0db5f72a316f8451aa34fd794ca952dec89f7788
Pyunghwa Kim <syneffort@gmail.com>
2020-06-03 20:14:02 +0900

    [#2] Compiler Optimize

--- 0_Sources/Server_Unity/ServerCore/Program.cs
+++ 0_Sources/Server_Unity/ServerCore/Program.cs
@@ -6,50 +6,35 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello Thread!");
+            Console.WriteLine("Thread Start");
+            
+            while (_stop == false)
+            {
+
+            }
+
+            Console.WriteLine("Thread Exit");
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } });
-                t.Start();
-            }
+            Thread.Sleep(1000);
 
-            //for (int i = 0; i < 4; i++)
-            //    ThreadPool.QueueUserWorkItem(obj => { while (true) { } });
+            _stop = true;
 
-            ThreadPool.QueueUserWorkItem(MainThread);
+            Console.WriteLine("Call Stop");
+            Console.WriteLine("Waiting for exit");
+            
+            t.Wait();
 
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Thread t = new Thread(MainThread);
-            //    //t.Name = "Test Thread";
-            //    t.IsBackground = true;
-            //    t.Start();
-            //}
-
-
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.IsBackground = true;
-            //t.Start();
-            //Console.WriteLine("Waiting for Thread");
-
-            //t.Join();
-            //Console.WriteLine("Hello World!");
-
-            while (true)
-            {
-
-            }
+            Console.WriteLine("Exit");
         }
     }
 }
