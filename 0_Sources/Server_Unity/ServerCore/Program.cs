9061fd4a3123a18ed56d5914108b65e9dce2cea2
Pyunghwa Kim <syneffort@gmail.com>
2020-06-04 07:24:16 +0900

    [#2] Interlocked

--- 0_Sources/Server_Unity/ServerCore/Program.cs
+++ 0_Sources/Server_Unity/ServerCore/Program.cs
@@ -4,60 +4,45 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // 메모리 배러어
-    // A) 코드 재배치 억제
-    // B) 가시성
-
-    // 1) Full Memory Barrier (ASM MFENCE) : Store/Load 모두 막음
-    // 2) Store Memory Barrier (ASM SFENCE) : Store 막음
-    // 3) LOAD Memory Barrier (ASM MFENCE) : Load 막음
-
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1;
+            // 원자성
+            for (int i = 0; i < 10000; i++)
+            {
+                Interlocked.Increment(ref number);
 
-            Thread.MemoryBarrier();
-
-            r1 = x;
+                //int temp = number;
+                //temp += 1;
+                //number = temp;
+            }
         }
 
         static void Thread_2()
         {
-            x = 1;
+            for (int i = 0; i < 10000; i++)
+            {
+                Interlocked.Decrement(ref number);
 
-            Thread.MemoryBarrier();
-
-            r2 = y;
+                //int temp = number;
+                //temp -= 1;
+                //number = temp;
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                x = y = r1 = r2 = 0;
+            Task.WaitAll(t1, t2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-
-            Console.WriteLine($"Exited at {count}-loop");
+            Console.WriteLine(number);
         }
     }
 }
