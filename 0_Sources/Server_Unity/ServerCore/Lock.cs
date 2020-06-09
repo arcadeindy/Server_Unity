95faecdcb2af05e7f58d889da184bf003ca0f0eb
Pyunghwa Kim <syneffort@gmail.com>
2020-06-10 08:36:31 +0900

    [#2] ReaderWriterLock Implementation 1/2

---
+++ 0_Sources/Server_Unity/ServerCore/Lock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    // 재귀적 락을 허용할지 (No)
+    // 스핀락 정책 (5000번 이상에서 yield)
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x00007FFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)] => 32bit
+        int _flag = EMPTY_FLAG;
+
+        public void WriteLock()
+        {
+            // 아무도 WriteLock or ReadLock을 획득하지 않았을 때 경합해서 소유권 획득
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    // 시도 중 성공하면 return
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                        return;
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        {
+            // 아무도 WriteLock을 획득하지 않았을 때 ReadCount를 1 늘림
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    if ((_flag & WRITE_MASK) == 0)
+                    {
+                        _flag += _flag;
+                        return;
+                    }
+
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+
+        }
+    }
+}
--- 0_Sources/Server_Unity/ServerCore/Program.cs
+++ 0_Sources/Server_Unity/ServerCore/Program.cs
@@ -7,68 +7,8 @@ namespace ServerCore
 {
     class Program
     {
-        // 1. 존버
-        // 2. 양보
-        // 3. 갑질
-
-        // 상호배제
-        // Monitor
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-        // or 직구현
-
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
-
-        class Reward
-        {
-
-        }
-
-        
-
-        static Reward GetRewardById(int id)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-
-            lock (_lock)
-            {
-
-            }
-            return null;
-        }
-
-        static void AddReward(Reward reward)
-        {
-            _lock3.EnterWriteLock();
-
-            _lock3.ExitWriteLock();
-
-            lock (_lock)
-            {
-
-            }
-        }
-
         static void Main(string[] args)
         {
-            lock (_lock)
-            {
-
-            }
-
-            bool lockTaken = false;
-            try
-            {
-                _lock2.Enter(ref lockTaken);
-            }
-            finally
-            {
-                if (lockTaken)
-                    _lock2.Exit();
-            }
-
             
         }
     }
