341fed226bb00b12c4a1ab419bcbb6599c9f5356
Pyunghwa Kim <syneffort@gmail.com>
2020-06-30 08:27:34 +0900

    [#4] Packet Generator 5/6

--- 0_Sources/Server_Unity/DummyClient/Packet/GenPackets.cs
+++ 0_Sources/Server_Unity/DummyClient/Packet/GenPackets.cs
@@ -11,7 +11,14 @@ public enum PacketID
 	
 }
 
-class PlayerInfoReq
+interface IPacket
+{
+	ushort Protocol { get; }
+	void Read(ArraySegment<byte> segment);
+	ArraySegment<byte> Write();
+}
+
+class PlayerInfoReq : IPacket
 {
     public byte testByte;
 	public long playerId;
@@ -82,6 +89,8 @@ class PlayerInfoReq
 	
 	public List<Skill> skills = new List<Skill>();
 
+    public ushort Protocol { get { return (ushort)PacketID.PlayerInfoReq; } }
+
     public void Read(ArraySegment<byte> segment)
     {
         ushort count = 0;
@@ -141,9 +150,11 @@ class PlayerInfoReq
     }
 }
 
-class Test
+class Test : IPacket
 {
     public int testInt;
+
+    public ushort Protocol { get { return (ushort)PacketID.Test; } }
 
     public void Read(ArraySegment<byte> segment)
     {
---
+++ 0_Sources/Server_Unity/Server/Packet/PacketHandler.cs
@@ -0,0 +1,22 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class PacketHandler
+    {
+        public static void PlayerInfoReqHandler(PacketSession session, IPacket packet)
+        {
+            PlayerInfoReq p = packet as PlayerInfoReq;
+
+            Console.WriteLine($"PlayerInfoReq : {p.playerId}, PlayerName : {p.name}");
+
+            foreach (PlayerInfoReq.Skill skill in p.skills)
+            {
+                Console.WriteLine($"Skill (Id : {skill.id}) (Lvl : {skill.level}) (Dur : {skill.duration})");
+            }
+        }
+    }
+}
