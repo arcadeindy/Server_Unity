5f62837153080e13f72e16fb076cd1cbe94c000d
Pyunghwa Kim <syneffort@gmail.com>
2020-07-02 08:39:50 +0900

    [#4] Packet Generator 6/6

--- 0_Sources/Server_Unity/Server/Packet/PacketHandler.cs
+++ 0_Sources/Server_Unity/Server/Packet/PacketHandler.cs
@@ -3,20 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Server
+class PacketHandler
 {
-    class PacketHandler
+    public static void C_PlayerInfoReqHandler(PacketSession session, IPacket packet)
     {
-        public static void PlayerInfoReqHandler(PacketSession session, IPacket packet)
+        C_PlayerInfoReq p = packet as C_PlayerInfoReq;
+
+        Console.WriteLine($"PlayerInfoReq : {p.playerId}, PlayerName : {p.name}");
+
+        foreach (C_PlayerInfoReq.Skill skill in p.skills)
         {
-            PlayerInfoReq p = packet as PlayerInfoReq;
-
-            Console.WriteLine($"PlayerInfoReq : {p.playerId}, PlayerName : {p.name}");
-
-            foreach (PlayerInfoReq.Skill skill in p.skills)
-            {
-                Console.WriteLine($"Skill (Id : {skill.id}) (Lvl : {skill.level}) (Dur : {skill.duration})");
-            }
+            Console.WriteLine($"Skill (Id : {skill.id}) (Lvl : {skill.level}) (Dur : {skill.duration})");
         }
     }
 }
